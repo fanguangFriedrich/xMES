910a258953b4955f75992c70ae36f935135b3ea5
薛绍友 <16835824+xue-shaoyou@user.noreply.gitee.com>
2026-05-19 10:17:02 +0800

    增加三色灯接口

--- OpenAuth.App/TriColorLamp/Response/TriColorLampResponse.cs
+++ OpenAuth.App/TriColorLamp/Response/TriColorLampResponse.cs
@@ -1,7 +1,20 @@
 using System.Text.Json.Serialization;
+using System.Collections.Generic;
 
 namespace OpenAuth.App.TriColorLamp.Response
 {
+    public class TriColorLampApiResponse<T>
+    {
+        [JsonPropertyName("code")]
+        public int Code { get; set; }
+
+        [JsonPropertyName("msg")]
+        public string Msg { get; set; }
+
+        [JsonPropertyName("data")]
+        public T Data { get; set; }
+    }
+
     public class TriColorLampTokenResponse
     {
         [JsonPropertyName("code")]
@@ -18,5 +31,50 @@ namespace OpenAuth.App.TriColorLamp.Response
     {
         [JsonPropertyName("token")]
         public string Token { get; set; }
+    }
+
+    public class TriColorLampStateInfoResponse
+    {
+        [JsonPropertyName("onlineCount")]
+        public int OnlineCount { get; set; }
+
+        [JsonPropertyName("offlineCount")]
+        public int OfflineCount { get; set; }
+
+        [JsonPropertyName("alarmCount")]
+        public int AlarmCount { get; set; }
+    }
+
+    public class TriColorLampRankingResponse
+    {
+        [JsonPropertyName("dtuSn")]
+        public string DtuSn { get; set; }
+
+        [JsonPropertyName("deviceName")]
+        public string DeviceName { get; set; }
+
+        [JsonPropertyName("stateCount")]
+        public int StateCount { get; set; }
+    }
+
+    public class TriColorLampDeviceResponse
+    {
+        [JsonPropertyName("projectState")]
+        public int ProjectState { get; set; }
+
+        [JsonPropertyName("projectType")]
+        public string ProjectType { get; set; }
+
+        [JsonPropertyName("dtuId")]
+        public long DtuId { get; set; }
+
+        [JsonPropertyName("dtuSn")]
+        public string DtuSn { get; set; }
+
+        [JsonPropertyName("deviceName")]
+        public string DeviceName { get; set; }
+
+        [JsonPropertyName("deviceId")]
+        public long DeviceId { get; set; }
     }
 }
--- OpenAuth.WebApi/Controllers/TriColorLampController.cs
+++ OpenAuth.WebApi/Controllers/TriColorLampController.cs
@@ -2,7 +2,9 @@ using Infrastructure;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OpenAuth.App.TriColorLamp;
+using OpenAuth.App.TriColorLamp.Response;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace OpenAuth.WebApi.Controllers
@@ -33,6 +35,68 @@ namespace OpenAuth.WebApi.Controllers
             try
             {
                 result.Data = await _app.GetValidAccessTokenAsync();
+            }
+            catch (Exception ex)
+            {
+                result.Code = 500;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 根据日期查询当日的正常数、离线数及告警数
+        /// </summary>
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<Response<TriColorLampStateInfoResponse>> GetStateInfo([FromQuery] string date)
+        {
+            var result = new Response<TriColorLampStateInfoResponse>();
+            try
+            {
+                result.Data = await _app.GetStateInfoAsync(date);
+            }
+            catch (Exception ex)
+            {
+                result.Code = 500;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 根据灯状态，获取当日灯状态排名
+        /// </summary>
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<Response<List<TriColorLampRankingResponse>>> GetRanking(
+            [FromQuery] int state,
+            [FromQuery] int ranking)
+        {
+            var result = new Response<List<TriColorLampRankingResponse>>();
+            try
+            {
+                result.Data = await _app.GetRankingAsync(state, ranking);
+            }
+            catch (Exception ex)
+            {
+                result.Code = 500;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 获取用户下灯设备列表
+        /// </summary>
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<Response<List<TriColorLampDeviceResponse>>> GetUserDtuSns()
+        {
+            var result = new Response<List<TriColorLampDeviceResponse>>();
+            try
+            {
+                result.Data = await _app.GetUserDtuSnsAsync();
             }
             catch (Exception ex)
             {
